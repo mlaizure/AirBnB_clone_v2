11bfe1cb9282417f8cabb884918b48184d6029d2
mlaizure <maddi.laizure@gmail.com>
2021-04-01 19:17:03 +0000

    testing setup change

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,6 @@
 -- Prepares MySQL development server
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+DROP USER IF EXISTS 'hbnb_dev'@'localhost';
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
 SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
