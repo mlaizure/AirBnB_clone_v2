c6dbb850d39fb9b26752eff94d9a839648dede75
mlaizure <maddi.laizure@gmail.com>
2021-04-15 05:34:45 +0000

    Changed dependencies

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,31 +1,37 @@
 # sets up web servers for deployment of web_static
 include stdlib
 
+
 exec { 'apt-get-update':
   command => '/usr/bin/env apt-get -y update',
 }
 
--> package { 'nginx':
+package { 'nginx':
   ensure  => 'installed',
+  require => Exec['apt-get-update'],
 }
 
--> file { [ '/data/', 'data/web_static/', 'data/web_static/shared/',
-            '/data/web_static/releases/', '/data/web_static/releases/test/' ]:
-              ensure => 'directory',
+file { 'make dirs':
+  ensure => 'directory',
+  path   => [ '/data/', 'data/web_static/', 'data/web_static/shared/',
+            '/data/web_static/releases/', '/data/web_static/releases/test/'],
 }
 
--> file { '/data/web_static/releases/test/index.html':
+file { '/data/web_static/releases/test/index.html':
   ensure  => 'file',
   content => 'simple content',
+  require => File['make dirs'],
 }
 
--> file { '/data/web_static/current':
-  ensure => 'link',
-  target => '/data/web_static/releases/test',
+file { '/data/web_static/current':
+  ensure  => 'link',
+  target  => '/data/web_static/releases/test',
+  require => File['/data/web_static/releases/test/index.html'],
 }
 
--> exec { 'chown':
+exec { 'chown':
   command => '/usr/bin/env chown -R ubuntu:ubuntu /data/',
+  require => File['/data/web_static/current'],
 }
 
 file_line { 'hbnb page config':
